ca39bdd465a18f8726520fd93dda0541d5072ec0
Jesse <jesse.mashoana@gmail.com>
2024-10-27 23:36:05 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const url = require('url');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const routes = {
+  'GET': {
+    '/': (req, res) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ message: 'Welcome to the homepage!' }));
+    },
+    '/about': (req, res) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ message: 'This is the about page.' }));
+    },
+    '/contact': (req, res) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ message: 'This is the contact page.' }));
+    }
+  },
+  'POST': {
+    '/data': (req, res) => {
+      let body = '';
+      req.on('data', chunk => {
+        body += chunk.toString();
+      });
+      req.on('end', () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ message: 'Data received', data: body }));
+      });
+    }
+  }
+};
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const routeHandler = routes[req.method]?.[parsedUrl.pathname];
+
+  if (routeHandler) {
+    routeHandler(req, res);
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ error: 'Not Found' }));
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
